b7ab50fec61e77dc477691f53ac319b3e0307f20
stevendulcey24 <stevendulcey0@gmail.com>
2023-01-03 15:30:41 -0500

    update final

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,4 @@
-import connection from './connection.js'
+import connection from '../db/connection.js'
 import authors from '../models/authors.js'
 import data from './authors.json' assert {type: 'json'}
 
